d9d905153ca8eabfaece5329b9d72a9cf64dca8f
Darsh <darshr2d2@gmail.com>
2025-01-05 16:16:25 -0800

    Changed Html Files into JSX

---
+++ about.jsx
@@ -0,0 +1,37 @@
+<>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>About Me</title>
+  <link rel="stylesheet" href="style.css" />
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <a href="main.html">Home</a>
+        </li>
+        <li>
+          <a href="about.html">About</a>
+        </li>
+        <li>
+          <a href="work.html">Work</a>
+        </li>
+        <li>
+          <a href="blog.html">Blog</a>
+        </li>
+        <li>
+          <a href="testimonials.html">Testimonials</a>
+        </li>
+        <li>
+          <a href="contact.html">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <section id="about" className="content-section">
+    <h1>About Me</h1>
+    <p>
+      My name is Darsh Gangakhedkar and I am a rising senior at University of
+      California Merced.
+    </p>
+  </section>
+</>
---
+++ work.jsx
@@ -0,0 +1,34 @@
+<>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>My Work</title>
+  <link rel="stylesheet" href="style.css" />
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <a href="main.html">Home</a>
+        </li>
+        <li>
+          <a href="about.html">About</a>
+        </li>
+        <li>
+          <a href="work.html">Work</a>
+        </li>
+        <li>
+          <a href="blog.html">Blog</a>
+        </li>
+        <li>
+          <a href="testimonials.html">Testimonials</a>
+        </li>
+        <li>
+          <a href="contact.html">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <section id="about" className="content-section">
+    <h1>My Work</h1>
+    <p>Here are my Projects!</p>
+  </section>
+</>
