2d7ae761c0b34b403cb04410c8919d66599926a5
Darsh <darshr2d2@gmail.com>
2025-01-30 15:22:57 -0800

    updated routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from 'react-router-dom'; // Import Router here
+import { BrowserRouter as Router } from 'react-router-dom'; // Import Router
 import App from './App';
 import './styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router> {/* Wrap the App component with Router */}
+  <Router basename="/darsh.github.io">  {/* Add basename for GitHub Pages */}
     <App />
   </Router>
 );
